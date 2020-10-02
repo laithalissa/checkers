c6f1db9e9f4cfed098f452c7745dec9d2bd49bef
Laith Alissa <laithalissa@gmail.com>
2020-10-02 22:35:55 +0100

    fix push message

--- refuse-slots/checker.py
+++ refuse-slots/checker.py
@@ -118,6 +118,6 @@ while True:
         run()
     except Exception as e:
         logger.exception('Unexpected error')
-        push('Error executing refuse checker: %s' % e)
+        push('Error executing refuse checker', e)
     logger.info('Sleeping for %d seconds', INTERVAL)
     time.sleep(INTERVAL)
